027eb6679b28316442c6f32abeae563caa6cb610
milos-p <milos.peulic@automaticity.rs>
2025-02-20 13:59:02 +0100

    constants, urls and header created

---
+++ fixtures/headers.js
@@ -0,0 +1,7 @@
+const HEADER = {
+  "x-api-key": `${process.env.API_KEY}`,
+  Accept: "application/json",
+  "Content-type": "application/json",
+};
+
+export { HEADER };
---
+++ fixtures/urls.js
@@ -0,0 +1,6 @@
+const URLS = {
+  API_BOOKS: "/api/books",
+  API_HEALTH: "/api/health",
+};
+
+export { URLS };
